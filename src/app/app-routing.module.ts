026079a168a32dec5307e413fea9b652de1f133e
cashastri <cashastri2016@gmail.com>
2018-06-14 00:06:22 +0530

    JS file loader added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,27 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
+
+function loadScript(url, id) {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement('script');
+    script.src = url;
+    if (id) {
+      script.id = id;
+    }
+    script.type="text/javascript";
+    script.async = false;
+    document.head.appendChild(script);
+    script.onload = function () {
+      typeof resolve == 'function' && resolve();
+    };
+
+    script.onerror = function () {
+      typeof reject == 'function' && reject();
+    };
+
+  });
+}
 
 const routes: Routes = [
   {
@@ -10,7 +31,12 @@ const routes: Routes = [
   {
     path: 'orders',
     // loadChildren: 'app/orders/orders.module#OrdersModule'
-    loadChildren: '/inventory-lib/bundles/inventory-lib.umd.js#OrdersModule'
+    loadChildren: async () => {
+      // '/inventory-lib/bundles/inventory-lib.umd.js#OrdersModule'
+      const script = await loadScript('/inventory-lib/bundles/inventory-lib.umd.js', 'inventory-lib');
+      const module = window['inventory-lib']['OrdersModule'];
+      return module;
+    }
   },
   {
     path: '',
@@ -23,4 +49,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
